d467c04aec3dd7bbdabd0a297d5338f98f663456
stale006 <stale006@fiu.edu>
2016-08-03 02:04:59 -0400

    Back End First Api

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var users = require('./routes/users');
 var design = require('./routes/design');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +26,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/topActiveUsers', users);
 app.use('/design', design);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var pg = require("pg");
 var router = express.Router();
 
-/* GET users listing. */
+var conString = "pg://bauizhhf5c:gdyiohbzp1@assignment.codsssqklool.eu-central-1.rds.amazonaws.com:5432/bauizhhf5c_db";
+
+var client = new pg.Client(conString);
+client.connect();
+
+/* GET active users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  var page = !!req.query.page && Number.isInteger( parseInt(req.query.page) ) ? parseInt(req.query.page)  : 1;
+  var per_page = 5;
+
+  //var query = client.query("SELECT id, created_at,name FROM users INNER JOIN applications ON applications.user_id = users.id");
+
+  var query = client.query(`
+    	SELECT users.id, users.created_at,	users.name, COUNT(applications.listing_id) AS count, array_agg(listings.name) AS listings
+    	FROM users
+    	LEFT JOIN applications ON users.id=applications.user_id AND applications.created_at > NOW() - INTERVAL '7 days'
+    	LEFT JOIN listings ON users.id=listings.created_by
+    	GROUP BY users.id
+      ORDER BY count DESC
+      LIMIT ${limit}
+    	OFFSET ${page}
+    	`);
+
+
+  query.on("row", function (row, result) {
+      result.addRow(row);
+  });
+  query.on("end", function (result) {
+      console.log(JSON.stringify(result.rows, null, "    "));
+      console.log();
+      client.end();
+      res.send('respond with a resource' + JSON.stringify(result.rows, null, "    ") );
+  });
+//res.send('respond with a resource' + page )
 });
 
 module.exports = router;
